ce7af6944ab226426923e44977c4c3fae7eb39cb
Ashley King <Princessak8918@hotmail.com>
2023-02-28 23:34:03 -0700

    pass test

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    return (customerName = customerName.toUpperCase());
+}
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+    window.bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'jim'
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'job';
+}
+Collapse
+
+
+
+eyes
+heavy_check_mark
+call_me_hand
+
+
+
+
+
+
+
+
+
+
+
+
+
